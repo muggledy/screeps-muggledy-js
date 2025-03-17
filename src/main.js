59a8d1833331132f9a66809445988de263af2a7f
muggledy <zgjsycfndy2015@163.com>
2025-03-17 23:12:15 +0800

    update Harvester: Memory

--- src/main.js
+++ src/main.js
@@ -1,38 +1,18 @@
 const utils = require('utils');
 
 function main() {
-    const creep_name = '_harvest0';
-    const creep = Game.creeps[creep_name];
-
-    if (creep) {
-        if (creep.store.getFreeCapacity() > 0) {
-            // creep 寻找资源并采集
-            const sources = creep.room.find(FIND_SOURCES_ACTIVE);
-            if (sources.length > 0) {
-                if (!creep.pos.isNearTo(sources[0])) {
-                    creep.moveTo(sources[0]);
-                } else {
-                    creep.harvest(sources[0]);
-                }
-            }
-        } else {
-            // 将 creep 携带的资源转移到 spawn
-            const spawns = creep.room.find(FIND_MY_SPAWNS);
-            if (spawns.length > 0) {
-                const spawn = _.sample(spawns);
-                if (!creep.pos.isNearTo(spawn)) {
-                    creep.moveTo(spawn);
-                } else {
-                    creep.transfer(spawn, RESOURCE_ENERGY);
-                }
-            }
+    const enter_time = Game.time;
+    console.log(`start ${enter_time}`);
+    utils.Harvester.print_all_harvesters();
+    /*Object.entries(Game.creeps).forEach(([name, creep]) => {
+        if (!utils.Harvester.is_registered(creep)) {
+            new utils.Harvester(creep);
         }
-    } else {
-        // 获取当前房间中随机一个 spawn
-        const one_spawn = utils.getRandomItemFromObject(Game.spawns);
-        // 生成一个新的 creep
-        one_spawn.spawnCreep([WORK, CARRY, MOVE], creep_name, { memory: { role: 'harvester' } });
-    }
+    })*/
+    utils.Harvester.get_alive_harvesters().forEach(harvester => {
+        harvester.run();
+    });
+    console.log(`end ${enter_time}`);
 }
 
 module.exports.loop = function () {
--- src/utils.js
+++ src/utils.js
@@ -8,23 +8,90 @@ function getRandomItemFromObject(obj) {
 const HARVESTER_ENERGY_TYPE = 0;
 
 class Harvester {
+    /*
+    Usage:
+    creep = spawn.spawnCreep([WORK, CARRY, MOVE], name, {'memory': {'role': 'harvester'}})
+    harvester = Harvester(creep)
+    harvester.run()
+    Note:
+    1) we can get the original creep object by `harvester.self()`
+    2) we can get all alive harvesters by Harvester.get_alive_harvesters()
+    中文描述：
+    当母巢生成一个(如资源)采集者creep，可以通过调用Harvester(creep)将其Harvester化，并返回一个harvester对象，新对象
+    具有两个好处，一是向Harvester管理机构进行了注册并且对其角色类型进行了更精细的划分，也更方便后续管理这些采集者，
+    二是Harvester管理机构提供了标准的运行动作供采集者执行，对于细分类型HARVESTER_ENERGY_TYPE角色的采集者来说，其标准动作为：
+    不断采集能量资源并搬运至母巢；
+    可通过harvester.self()获取原始creep对象（如果creep对象已经消亡，则返回None）；可通过Harvester.get_alive_harvesters()
+    获取所有注册了的且当前仍存活的harvester对象列表，如果type参数非None，则获取指定细分类型的harvester列表
+    */
+
     // 静态属性，用于存储已注册的 harvester 实例
-    static nameids = {};
+    static get nameids() {
+        // 检查 Memory 中是否已经存在容器
+        if (!Memory.harvesterNameIds) {
+            Memory.harvesterNameIds = {}; //Memory持久化存储，但是存储的不是harvester本身，而是序列化后的普通object（即非Harvester实例）
+        }
+        return Memory.harvesterNameIds;
+    }
+    static nameids_non_persistent = {}; //非持久化存储、临时存储，存储harvester实例本身
+                                        // 对nameids、nameids_non_persistent的增删改查请使用addData、removeData、modifyDataProperty、getData
+
+    static addData(name, harvester) {
+        Harvester.nameids[name] = harvester;
+        Harvester.nameids_non_persistent[name] = harvester;
+    }
+
+    static getData(name) {
+        const harvester = Harvester.nameids_non_persistent[name];
+        if (harvester) {
+            return harvester;
+        }
+        const obj = Harvester.nameids[name];
+        if (obj) {
+            const new_harvester = Harvester.fromObject(obj);
+            Harvester.nameids_non_persistent[new_harvester.name] = new_harvester;
+            return new_harvester;
+        }
+        return undefined;
+    }
+
+    static removeData(name) {
+        delete Harvester.nameids[name];
+        delete Harvester.nameids_non_persistent[name];
+    }
+
+    static modifyDataProperty(name, property, value) { //modify nameids[name].property = value
+        // 检查字典中是否存在指定的键
+        if (Harvester.getData(name)) {
+            Harvester.nameids[name][property] = value;
+            Harvester.nameids_non_persistent[name][property] = value;
+        }
+    }
+
+    static fromObject(obj) { //根据持久化数据恢复出harvester对象，入参是从持久化Memory.nameids中取的普通obj
+        const harvester = Object.create(Harvester.prototype);
+        // 在这里可以根据需要将obj中的属性复制到新的实例中
+        harvester.name = obj.name;
+        harvester.type = obj.type;
+        harvester.id = obj.id;
+        return harvester;
+    }
 
     constructor(creep, type = HARVESTER_ENERGY_TYPE) {
         // 检查是否已经存在该 creep 名字对应的实例
-        if (Harvester.nameids[creep.name]) {
-            Harvester.nameids[creep.name].name = creep.name;
-            Harvester.nameids[creep.name].type = type;
-            Harvester.nameids[creep.name].id = creep.id;
-            return Harvester.nameids[creep.name];
+        if (Harvester.getData(creep.name)) {
+            Harvester.modifyDataProperty(creep.name, 'name', creep.name);
+            Harvester.modifyDataProperty(creep.name, 'type', type);
+            Harvester.modifyDataProperty(creep.name, 'id', creep.id);
+            return Harvester.getData(creep.name);
         }
-        // 若不存在，则创建新实例
+        // 若不存在，则创建新harvester实例
+        console.log(`create new harvester for new register one creep(${creep.name},type:${type},id:${creep.id})`);
         this.name = creep.name;
         this.type = type;
         this.id = creep.id;
         // 将新实例存储到 nameids 中
-        Harvester.nameids[creep.name] = this;
+        Harvester.addData(creep.name, this);
     }
 
     run() {
@@ -34,12 +101,12 @@ class Harvester {
     }
 
     run_for_harvester_energy_type() {
-        const creep = this.self();
+        const creep = this.self(); //i.e., this.creep()
         if (!creep) {
             return;
         }
         if (creep.store.getFreeCapacity() > 0) {
-            // continue to find resource and do harvest
+            // continue to find resource and do harvest（身上的容器未满则creep继续寻找资源并采集）
             const sources = creep.room.find(FIND_SOURCES_ACTIVE);
             if (sources.length > 0) {
                 if (!creep.pos.isNearTo(sources[0])) {
@@ -49,10 +116,10 @@ class Harvester {
                 }
             }
         } else {
-            // transfer resource the creep carry to spawn
+            // transfer resource the creep carry to spawn（否则creep将携带的资源转移给spawn）
             const spawns = creep.room.find(FIND_MY_SPAWNS);
             if (spawns.length > 0) {
-                const spawn = _.sample(spawns); // get a random spawn
+                const spawn = getRandomItemFromObject(spawns); // get a random spawn
                 if (!creep.pos.isNearTo(spawn)) {
                     creep.moveTo(spawn);
                 } else {
@@ -63,7 +130,7 @@ class Harvester {
     }
 
     is_alive() {
-        return Game.creeps.hasOwnProperty(this.name) && this.id === Game.creeps[this.name].id;
+        return (Game.creeps.hasOwnProperty(this.name) && (this.id === Game.creeps[this.name].id));
     }
 
     self() {
@@ -72,22 +139,54 @@ class Harvester {
         }
         return null;
     }
+    creep() {
+        if (this.is_alive()) {
+            return Game.creeps[this.name];
+        }
+        return null;
+    }
 
     static get_alive_harvesters(type = null) {
         const harvesters = [];
-        for (const [name, harvester] of Object.entries(Harvester.nameids)) {
+        let harvester = null;
+        for (const [name, obj] of Object.entries(Harvester.nameids)) {
+            harvester = Harvester.getData(name);
             if (harvester.is_alive()) {
-                if (type === null || harvester.type === type) {
+                if ((type === null) || (harvester.type === type)) {
                     harvesters.push(harvester);
                 }
             } else {
-                delete Harvester.nameids[name];
+                Harvester.removeData(name);
             }
         }
         return harvesters;
     }
+
+    static is_registered(creep) { //判断一个creep是否已经做过Harvester化（即是否已经注册过）
+        let harvester = null;
+        for (const [name, obj] of Object.entries(Harvester.nameids)) {
+            harvester = Harvester.getData(name);
+            if (harvester.is_alive() && (creep.id === harvester.id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static print_all_harvesters(type = null) {
+        let i = 1;
+        let harvester = null;
+        for (const [name, obj] of Object.entries(Harvester.nameids)) {
+            harvester = Harvester.getData(name);
+            if ((type === null) || (obj.type === type)) {
+                console.log(`harvester-${i}: name:${name}, type:${obj.type}, id:${obj.id}, is_alive:${harvester.is_alive()}`);
+                i += 1;
+            }
+        }
+    }
 }
 
 module.exports = {
-    getRandomItemFromObject: getRandomItemFromObject
+    getRandomItemFromObject, 
+    Harvester, HARVESTER_ENERGY_TYPE
 };
